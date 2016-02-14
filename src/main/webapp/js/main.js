4b4abf2fe1d19713d787a37fc7e40531948c90d6
pivs96 <jpalaznik@gmail.com>
2016-02-14 20:05:35 +0300

    Add arrow keypresses listening

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -5,8 +5,8 @@ $(function () {
         Collections: {}
     };
 
-    window.App.template = function(id) {
-        return _.template( $('#' + id).html() );
+    window.App.plyTemplate = function () {
+        return _.template("<%= commentBefore %> <strong><%= indexString %></strong> <%= commentAfter %>");
     };
 
     window.App.hash = [
@@ -21,7 +21,13 @@ $(function () {
     ];
 
     App.Views.Position = Backbone.View.extend({
-        render: function (position) {
+        brdSize: 8,
+        initialize: function () {
+            this.model.on('change', this.render, this);
+            this.render();
+        },
+        render: function () {
+            var position = this.model;
             var whites = position.get('whites');
             var blacks = position.get('blacks');
             var whiteKings = position.get('whiteKings');
@@ -29,22 +35,10 @@ $(function () {
             for (var i = 1; i <= this.brdSize * this.brdSize / 2; i++) {
                 this.clearSquare(i);
             }
-            for ( i = 0; i < whites.length; i++) {
-                this.putFigure(whites[i], 'white-figure');
-            }
-            for ( i = 0; i < blacks.length; i++) {
-                this.putFigure(blacks[i], 'black-figure');
-            }
-            for ( i = 0; i < whiteKings.length; i++) {
-                this.putFigure(whiteKings[i], 'white-queen');
-            }
-            for ( i = 0; i < blackKings.length; i++) {
-                this.putFigure(blackKings[i], 'black-queen');
-            }
-        },
-        putFigure: function (coordinate10x10, className) {
-            var coordinate8x8 = App.hash[coordinate10x10 - 1];
-            $('#' + coordinate8x8 + '-square').addClass(className);
+            this.setDraughts(whites, 'white-figure');
+            this.setDraughts(blacks, 'black-figure');
+            this.setDraughts(whiteKings, 'white-queen');
+            this.setDraughts(blackKings, 'black-queen');
         },
         clearSquare: function (i) {
             var coordinate8x8 = App.hash[i - 1];
@@ -53,6 +47,12 @@ $(function () {
             $coordSquare.removeClass('black-figure');
             $coordSquare.removeClass('white-queen');
             $coordSquare.removeClass('black-queen');
+        },
+        setDraughts: function (draughts, className) {
+            for (var i = 0; i < draughts.length; i++) {
+                var coordinate8x8 = App.hash[draughts[i] - 1];
+                $('#' + coordinate8x8 + '-square').addClass(className);
+            }
         }
     });
 
@@ -61,8 +61,7 @@ $(function () {
         whiteKings: [],
         blacks: [],
         blackKings: [],
-        //isWhiteMove: true
-        whiteMove: true,
+        isWhiteMove: true
     });
 
     App.Models.Ply = Backbone.Model.extend({
@@ -88,24 +87,24 @@ $(function () {
     App.Views.Ply = Backbone.View.extend({
         tagName: 'span',
         className: 'ply',
-        template: App.template('plyTemplate'),
+        template: App.plyTemplate(),
         events: {
-            'click > strong' : 'selectPly'
+            'click > strong': 'selectPly'
         },
 
-        selectPly: function() {
+        selectPly: function () {
             alert("A ply was clicked!");
         },
-        render: function() {
+        render: function () {
             var ply = this.model;
             var plyNotation = this.getPlyNotation(ply);
             var plyLayout;
-            if (!ply.get("position").get('whiteMove')) {
+            if (!ply.get("position").get('isWhiteMove')) {
                 plyLayout = ply.get('index') + ". " + plyNotation;
             } else
-                plyLayout = ply.get('index')+ ". ... " + plyNotation;
+                plyLayout = ply.get('index') + ". ... " + plyNotation;
             ply.set('indexString', plyLayout);
-            this.$el.html( this.template(ply.toJSON()));
+            this.$el.html(this.template(ply.toJSON()));
             return this;
         },
         getPlyNotation: function (ply) {
@@ -118,17 +117,17 @@ $(function () {
     App.Views.Plies = Backbone.View.extend({
         tagName: 'div',
 
-        initialize: function(){
+        initialize: function () {
             this.collection.on('add', this.addOne, this);
         },
 
-        render: function(){
+        render: function () {
             this.collection.each(this.addOne, this);
             return this;
         },
 
-        addOne: function(ply){
-            var plyView = new App.Views.Ply({ model: ply });
+        addOne: function (ply) {
+            var plyView = new App.Views.Ply({model: ply});
             this.$el.append(plyView.render().el);
         }
     });
@@ -148,19 +147,17 @@ $(function () {
                 success: function (form) {
                     var game = form.attributes;
                     context.begin = new App.Models.Position(game.begin);
-                    if (!context.begin.get('whiteKings')) context.begin.set('whiteKings',[]);
-                    if (!context.begin.get('blackKings')) context.begin.set('blackKings',[]);
-                    if (!context.begin.get('whites')) context.begin.set('whites',[]);
-                    if (!context.begin.get('blacks')) context.begin.set('blacks',[]);
+                    if (!context.begin.get('whiteKings')) context.begin.set('whiteKings', []);
+                    if (!context.begin.get('blackKings')) context.begin.set('blackKings', []);
+                    if (!context.begin.get('whites')) context.begin.set('whites', []);
+                    if (!context.begin.get('blacks')) context.begin.set('blacks', []);
 
                     var plies = context.getPliesArray(game);
                     context.pliesCollection = new App.Collections.Plies(plies);
-                    var pliesView = new App.Views.Plies({collection: context.pliesCollection });
+                    var pliesView = new App.Views.Plies({collection: context.pliesCollection});
                     $('#game-plies').append(pliesView.render().el);
 
-                    context.positionView = new App.Views.Position({model: context.begin});
-                    context.positionView.render(context.begin);
-
+                    context.positionView = new App.Views.Position({model: new App.Models.Position(context.begin.toJSON())});
                 }
             });
         },
@@ -194,9 +191,8 @@ $(function () {
                     return "white";
         },
         determineSquareCoordinate: function (i, j) {
-            return App.hash[
-            (this.brdSize + 1 - i - 1) * 4 + Math.ceil(j / 2) - 1
-                ];
+            var index = (this.brdSize + 1 - i - 1) * 4 + Math.ceil(j / 2) - 1;
+            return App.hash[index];
         },
         getSquareClassName: function () {
             return 'board8x8-square';
@@ -206,30 +202,31 @@ $(function () {
         },
         events: {
             "click #next-step": "nextStep",
-            "click #prev-step": "prevStep"
+            "click #prev-step": "prevStep",
         },
         nextStep: function () {
             if (this.currentPlyIndex < this.pliesCollection.length) {
                 ++this.currentPlyIndex;
-                var position = this.pliesCollection.at(this.currentPlyIndex).get('position');
-                this.positionView.render(position);
+                var position = this.pliesCollection.at(this.currentPlyIndex - 1).get('position');
+                this.positionView.model.set(position.toJSON());
             }
         },
         prevStep: function () {
-            if (this.currentPlyIndex > 0) {
-                --this.currentPlyIndex;
-                var position = this.pliesCollection.at(this.currentPlyIndex).get('position');
-                this.positionView.render(position);
+            if (this.currentPlyIndex <= 0) {
+                return;
             }
+            --this.currentPlyIndex;
+            var position = this.begin;
+            if (this.currentPlyIndex != 0) {
+                position = this.pliesCollection.at(this.currentPlyIndex - 1).get('position');
+            }
+            this.positionView.model.set(position.toJSON());
         },
-
-
         getPliesArray: function (response) {
             var parsedMoves = [];
             var moves = response["mainMoves"]["moves"];
             for (var i = 0; i < moves.length; i++) {
                 var ply = moves[i];
-                //if (!ply["alternatives"]) ply["alternatives"] = [];
                 if (!ply["plyPosition"]["position"].whites) ply["plyPosition"]["position"].whites = [];
                 if (!ply["plyPosition"]["position"].whiteKings) ply["plyPosition"]["position"].whiteKings = [];
                 if (!ply["plyPosition"]["position"].blacks) ply["plyPosition"]["position"].blacks = [];
@@ -276,12 +273,26 @@ $(function () {
         }
     });
 
-    App.Views.Game8x8 = App.Views.Game.extend({});
-
     var game_positions = new App.Collections.Plies();
     //window.draughts_board_demonstration = new App.Views.Game10x10({collection: game_positions, el: '#draughts-board-demonstration'});
     window.draughts_board_demonstration = new App.Views.Game({
         collection: game_positions,
         el: '#draughts-board-demonstration'
     });
+
+    $(document).keydown(function(e) {
+        switch(e.which) {
+            case 37: // left
+                window.draughts_board_demonstration.prevStep();
+                break;
+
+            case 39: // right
+                window.draughts_board_demonstration.nextStep();
+                break;
+
+            default: return; // exit this handler for other keys
+        }
+        e.preventDefault();
+    });
+
 });
